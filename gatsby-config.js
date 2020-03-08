43531a177f77212ada2f10cbe90099e9515e83f3
Noel Torres <noletorious@Noels-MacBook-Pro.local>
2020-03-08 14:08:07 -0700

    Add bootstrap, creating working image dir and ignore it

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Noel Torres - Interaction Designer`,
+    description: ``,
+    author: `@noletorious`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
